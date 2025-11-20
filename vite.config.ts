df8c75fe5796050a7700425337deacbb711f4827
LepkoQQ <lepko.san@gmail.com>
2025-11-20 19:55:17 +0100

    make base url relative in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: './',
       root: path.resolve(__dirname, 'src'),
       build: {
         outDir: path.resolve(__dirname, 'dist'),
