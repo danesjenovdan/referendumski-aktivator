bfe3de6e19f83e551b2b12e898bba5dfc91f002d
Filip Muki Dobranic <filipdobranic@gmail.com>
2025-11-22 00:04:42 +0100

    update checklist items

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const INITIAL_ITEMS: ChecklistItem[] = [
     text: 'Poišči svoje volišče',
     description: 'Veš, kje moraš oddati glas? Preveri na vabilu ali e-Upravi.',
     isCompleted: false,
-    impact: 5,
+    impact:10,
     link: 'https://www.dvk-rs.si/volisca/',
     linkText: 'Iskalnik volišč'
   },
@@ -15,7 +15,7 @@ export const INITIAL_ITEMS: ChecklistItem[] = [
     text: 'Preveri veljavnost dokumenta',
     description: 'Brez veljavne osebne izkaznice ali potnega lista ne moreš glasovati. Pripravi si ga danes.',
     isCompleted: false,
-    impact: 10
+    impact: 5
   },
   {
     id: '3',
@@ -31,19 +31,19 @@ export const INITIAL_ITEMS: ChecklistItem[] = [
     isCompleted: false,
     impact: 25
   },
-  {
-    id: '5',
-    text: 'Objavi na družbenih omrežjih',
-    description: 'Svojo podporo zakonu objavi na socialnih omrežjih. Zgled šteje!',
-    isCompleted: false,
-    impact: 15
-  },
+  // {
+  //   id: '5',
+  //   text: 'Objavi na družbenih omrežjih',
+  //   description: 'Svojo podporo zakonu objavi na socialnih omrežjih. Zgled šteje!',
+  //   isCompleted: false,
+  //   impact: 15
+  // },
   {
     id: '7',
     text: 'Dodaj si digitalno priponko',
     description: 'Na družbenih omrežjih si lahko k profilni sliki dodaš priponko podpore.',
     isCompleted: false,
-    impact: 10,
+    impact: 15,
     link: 'https://vodici.djnd.si/referendum/dostojna-smrt/priponka-podpore/',
     linkText: 'Priponka podpore'
   },
